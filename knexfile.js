f61152d60377f5a2033a7be41029188c97db532e
mike hill <michael-hill@lambdastudents.com>
2020-11-13 19:34:09 -0500

    fixxed postgres thing

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   }
 };
